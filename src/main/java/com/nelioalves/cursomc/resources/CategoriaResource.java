50dc9fd4167e1b4e26fdebefa8af461e29b88d79
fakebmarchete <marcosrodrigues200@msn.com>
2018-08-14 14:14:36 -0300

    Alteracao no retorno

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
-		return "Rest está funcionado";
+		return "Rest está funcion";
 	}
 
 }
