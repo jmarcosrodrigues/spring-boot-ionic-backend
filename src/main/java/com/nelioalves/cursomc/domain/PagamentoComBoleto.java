cae8d83e9cd04f132352ea89557dfda2b9a4c216
fakebmarchete <marcosrodrigues200@msn.com>
2019-02-19 21:01:19 -0300

    inserindo pedido

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -5,9 +5,11 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
@@ -22,7 +24,6 @@ public class PagamentoComBoleto extends Pagamento {
 	}
 
 	
-	
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
 		this.dataVencimento = dataVencimento;
@@ -31,11 +32,9 @@ public class PagamentoComBoleto extends Pagamento {
 	}
 
 
-
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
-
 
 
 	public void setDataVencimento(Date dataVencimento) {
@@ -43,19 +42,13 @@ public class PagamentoComBoleto extends Pagamento {
 	}
 
 
-
 	public Date getDataPagamento() {
 		return dataPagamento;
 	}
 
 
-
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-
-
-	
-	
 
 }
